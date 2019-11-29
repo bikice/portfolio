5ca6fc1877427bcfffb31acb99876bab59bf5184
Kristof Kamin <kristof.kamin@gmail.com>
2019-11-29 18:19:16 +0100

    packages

--- Gruntfile.js
+++ Gruntfile.js
@@ -2,8 +2,6 @@ module.exports = function(grunt) {
 
   grunt.loadNpmTasks('grunt-contrib-sass');
   grunt.loadNpmTasks('grunt-contrib-uglify-es');
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-qunit');
   grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-copy');
